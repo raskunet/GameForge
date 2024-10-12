20045828d993ed7b7599e2a5b7cbdd44da24c0be
raskunet <111647493+raskunet@users.noreply.github.com>
2024-10-12 16:04:43 +0500

    Modified By ef migrations

--- Data/GameForgeContext.cs
+++ Data/GameForgeContext.cs
@@ -13,7 +13,39 @@ namespace GameForge.Data
             : base(options)
         {
         }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
 
-        public DbSet<GameForge.Models.User> User { get; set; } = default!;
+            modelBuilder.Entity<ThreadTopicReply>()
+                .HasOne(e => e.ThreadTopic)
+                .WithMany(e => e.ThreadTopidcReplies)
+                .HasForeignKey(e => new { e.ThreadTopicID, e.UserID })
+                .IsRequired();
+
+            modelBuilder.Entity<ThreadTopic>()
+                .HasOne(e => e.User)
+                .WithMany(e => e.ThreadTopics)
+                .HasForeignKey(e => e.UserID)
+                .IsRequired();
+
+            modelBuilder.Entity<Question>()
+                .HasOne(e => e.User)
+                .WithMany(e => e.Questions)
+                .HasForeignKey(e => e.AuthorID)
+                .IsRequired();
+
+            modelBuilder.Entity<Answer>()
+                .HasOne(e => e.Question)
+                .WithMany(e => e.Answers)
+                .HasForeignKey(e => new { e.UserID, e.QuestionID })
+                .IsRequired();
+
+        }
+
+        public DbSet<User> User { get; set; } = default!;
+        public DbSet<ThreadTopic> ThreadTopic { get; set; }
+        public DbSet<ThreadTopicReply> ThreadTopicReplies{ get; set; }
+        public DbSet<GameForge.Models.Question> Question { get; set; } = default!;
+        public DbSet<GameForge.Models.Answer> Answer { get; set; } = default!;
     }
 }
